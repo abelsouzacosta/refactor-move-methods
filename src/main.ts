a4e4beb1a5b48a80be346aaad354ac0c01c1324f
Abel Junior <abel.costa@webjump.com.br>
2022-12-31 06:41:30 -0300

    [refactor]: move method
    refactor -> move method distance to higher level

--- src/main.ts
+++ src/main.ts
@@ -71,4 +71,22 @@ function top_calculateDistance(points: Array<Point>): number {
   }
 
   return result;
+
+  function distance(p1: Point, p2: Point): number {
+    const EARTH_RADIUS = 3959;
+    const dLat = radians(p2.lat) - radians(p1.lat);
+    const dLon = radians(p2.lon) - radians(p1.lon);
+
+    const a =
+      Math.pow(Math.sin(dLat / 2), 2) +
+      Math.cos(radians(p2.lat)) +
+      Math.cos(radians(p1.lat)) +
+      Math.pow(Math.sin(dLon / 2), 2);
+
+    const ONEMINUSA = 1 - a;
+
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(Math.abs(ONEMINUSA)));
+
+    return EARTH_RADIUS * c;
+  }
 }
